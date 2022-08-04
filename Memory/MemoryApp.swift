4a22c184b9234ecc7f272e2d33ac9b20ed3e856d
Radharani Valongo <radharanivalongo@pursuit.org>
2022-08-04 11:47:33 -0400

    Initial Commit

---
+++ Memory/MemoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryApp.swift
+//  Memory
+//
+//  Created by Rod on 8/4/22.
+//
+
+import SwiftUI
+
+@main
+struct MemoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
